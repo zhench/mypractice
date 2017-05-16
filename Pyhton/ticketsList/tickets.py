1734094a72c16ecfeda3c8fc76121e9d70368c97
shuguid <ding_zhenchuan@163.com>
2017-05-16 23:40:53 +0800

    tickets.py

--- Pyhton/ticketsList/tickets.py
+++ Pyhton/ticketsList/tickets.py
@@ -6,7 +6,23 @@ Usage:
     
 Options:
     -h,--help   显示帮助菜单
-
-
+    -g          高铁
+    -d          动车
+    -t          特快
+    -k          快速
+    -z          直达
+    
+Example：
+    tickets 北京 上海 2016-10-10
+    tickets -dg 成都 南京 2016-10-10
 """
 from docopt import docopt
+
+def cli():
+    """command-line interface"""
+    arguments=docopt(__doc__)
+    print(arguments);
+
+if __name__=='__main__':
+    cli()
+
